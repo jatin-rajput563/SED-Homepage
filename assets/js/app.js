50a17ac707378555d44c5f2872cab50521474ca9
jatin-rajput563 <jkt3298@gmail.com>
2025-03-12 11:37:16 +0530

    section-2-ing-progress

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,3 +5,38 @@ const navbarbtn = () => {
     document.getElementById("rotates-2").classList.toggle("rotate-45")
     document.body.classList.toggle("overflow-hidden")
 }
+$('.slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 8,
+    slidesToScroll: 8,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 4,
+                slidesToScroll: 4,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
+});
